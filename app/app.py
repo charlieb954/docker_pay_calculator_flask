7c9b50b8088910e092e04055eb13e5f5e42cd5b2
Charlie B <charlie@ubuntu>
2021-02-11 03:42:25 -0800

    add route for contact page

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for
 
 app = Flask(__name__)
 
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def home():
     hw = "hello world!"
     return render_template("home.html", hw=hw)
+
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
