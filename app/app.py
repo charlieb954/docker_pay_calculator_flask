f75aeaf4ce88639c089ca72232a147e66941a99f
Charlie B <charlie@ubuntu>
2021-02-12 03:54:12 -0800

    add home route

--- app/app.py
+++ app/app.py
@@ -2,6 +2,10 @@ from flask import Flask, render_template, request, url_for, jsonify, flash
 
 app = Flask(__name__)
 
+@app.route('/')
+def home():
+    return render_template("home.html")
+
 @app.route('/contact')
 def contact():
     return render_template("contact.html")
