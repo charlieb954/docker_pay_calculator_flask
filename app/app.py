a9384398f1aa57b4d4487230955a12ce803c6a17
Charlie B <charlie@ubuntu>
2021-02-26 02:51:45 -0800

    change format for deductions to float @

--- app/app.py
+++ app/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, url_for, jsonify, flash
 from takehomepay import national_insurance_20, tax_pens_20
+from takehomepay.utils import thousands_format, thousands_to_float
 
 app = Flask(__name__)
 
@@ -18,13 +19,11 @@ def my_form_post():
         salary = int(request.form['salary'])
     except ValueError:
         return render_template("home.html", error="Please check your input and try again.") 
-    if salary >= 100000:
-        return 'This is only programmed for salaries up to 100k.'
-        
-    salary_year = str("%.2f" %salary)
-    salary_month = str("%.2f" %(salary/12))
-    salary_week = str("%.2f" %(salary/(365/7)))
-    salary_day = str("%.2f" %(salary/365))
+
+    salary_year = salary
+    salary_month = salary / 12
+    salary_week = salary / 52
+    salary_day = salary / 365
 
     try:
         pension = int(request.form['pension'])
@@ -33,51 +32,46 @@ def my_form_post():
 
     tax_year, pension_year = tax_pens_20(salary, pension)
 
-    tax_month = str("%.2f" %(float(tax_year) / 12))
-    tax_week = str("%.2f" %(float(tax_year) / (365/7)))
-    tax_day = str("%.2f" %(float(tax_year) / 365))
+    tax_month = tax_year / 12
+    tax_week = tax_year / 52
+    tax_day = tax_year / 365
 
-    pension_month = str("%.2f" %(float(pension_year) / 12))
-    pension_week = str("%.2f" %(float(pension_year) / (365/7)))
-    pension_day = str("%.2f" %(float(pension_year) / 365))
+    pension_month = pension_year / 12
+    pension_week = pension_year / 52
+    pension_day = pension_year / 365
 
     ni_week = national_insurance_20(salary)
-    ni_year = str("%.2f" %(float(ni_week)*(365/7)))
-    ni_month = str("%.2f" %((float(ni_week)*(365/7))/12))
-    ni_day = str("%.2f" %(float(ni_week)/7))
+    ni_year = ni_week * 52
+    ni_month = (ni_week*52) / 12
+    ni_day = ni_week / 7
 
-    take_home_year = float(salary_year) - float(tax_year) - float(pension_year) -  float(ni_year)
-    take_home_month = float(salary_month) - float(tax_month) - float(pension_month) -  float(ni_month)
-    take_home_week = float(salary_week) - float(tax_week) - float(pension_week) -  float(ni_week)
-    take_home_day = float(salary_day) - float(tax_day) - float(pension_day) -  float(ni_day)
-
-    take_home_year = str("%.2f" % take_home_year)
-    take_home_month = str("%.2f" % take_home_month)
-    take_home_week = str("%.2f" % take_home_week)
-    take_home_day = str("%.2f" % take_home_day)
+    take_home_year = salary_year - tax_year - pension_year - ni_year
+    take_home_month = salary_month - tax_month - pension_month - ni_month
+    take_home_week = salary_week - tax_week - pension_week - ni_week
+    take_home_day = salary_day - tax_day - pension_day -  ni_day
 
 
     return render_template('results.html',
-                            salary_year = salary_year,
-                            salary_month = salary_month,
-                            salary_week = salary_week,
-                            salary_day = salary_day,
-                            pension_year = pension_year,
-                            pension_month = pension_month,
-                            pension_week = pension_week,
-                            pension_day = pension_day,
-                            tax_year = tax_year,
-                            tax_month = tax_month,
-                            tax_week = tax_week,
-                            tax_day = tax_day,
-                            ni_year = ni_year,
-                            ni_month = ni_month,
-                            ni_week = ni_week,
-                            ni_day = ni_day,
-                            take_home_year = take_home_year,
-                            take_home_month = take_home_month,
-                            take_home_week = take_home_week,
-                            take_home_day = take_home_day)
+                            salary_year = thousands_format(salary_year),
+                            salary_month = thousands_format(salary_month),
+                            salary_week = thousands_format(salary_week),
+                            salary_day = thousands_format(salary_day),
+                            pension_year = thousands_format(pension_year),
+                            pension_month = thousands_format(pension_month),
+                            pension_week = thousands_format(pension_week),
+                            pension_day = thousands_format(pension_day),
+                            tax_year = thousands_format(tax_year),
+                            tax_month = thousands_format(tax_month),
+                            tax_week = thousands_format(tax_week),
+                            tax_day = thousands_format(tax_day),
+                            ni_year = thousands_format(ni_year),
+                            ni_month = thousands_format(ni_month),
+                            ni_week = thousands_format(ni_week),
+                            ni_day = thousands_format(ni_day),
+                            take_home_year = thousands_format(take_home_year),
+                            take_home_month = thousands_format(take_home_month),
+                            take_home_week = thousands_format(take_home_week),
+                            take_home_day = thousands_format(take_home_day))
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
