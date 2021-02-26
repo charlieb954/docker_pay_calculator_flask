2b4d4866a0d50e24e70b07f1318eb310d43ab37f
Charlie B <charlie@ubuntu>
2021-02-26 02:52:39 -0800

    update calculation for ni

--- app/takehomepay/takehomepay.py
+++ app/takehomepay/takehomepay.py
@@ -1,3 +1,5 @@
+from takehomepay.utils import thousands_format
+
 __all__ = ['tax_pens_20',
         'national_insurance_20']
 
@@ -12,50 +14,47 @@ def tax_pens_20(sal, pens):
 
     returns
     -------
-    str: total_tax to pay, str: pension_contr to pay
+    float: total_tax to pay, float: pension_contr to pay
     """
     pension_contr = sal * (pens/100)
     allowance = 12500
     sal = sal - pension_contr
 
-    if sal <= allowance:
-        return str("%.2f" %0), str("%.2f" %pension_contr)
+    if sal < 0:
+        return 'Unknown'
+
+    elif sal <= allowance:
+        total_tax = 0
 
     elif sal in range(12501,50001):
         tax1 = 0 * allowance
         tax2 = 0.2 * (sal - allowance)
         total_tax = tax1 + tax2
-        return str("%.2f" %total_tax), str("%.2f" %pension_contr)
 
     elif sal in range(50001,100001):
         tax1 = 0 * allowance
         tax2 = 0.2 * 37500
         tax3 = 0.4 * (sal - 50000)
         total_tax = tax1 + tax2 + tax3
-        return str("%.2f" %total_tax), str("%.2f" %pension_contr)
 
     elif sal in range(100001,125000):
         allowance = (sal - 100000) / 2
         tax1 = 0.2 * 37500
         tax2 = 0.4 * (sal - (37500 + allowance))
         total_tax = tax1 + tax2
-        return str("%.2f" %total_tax), str("%.2f" %pension_contr)
 
     elif sal in range(125000,150001):
         tax1 = 0.2 * 37500
         tax2 = 0.4 * (sal - 37500)
         total_tax = tax1 + tax2
-        return str("%.2f" %total_tax), str("%.2f" %pension_contr)
 
-    elif sal >= 150001:
+    else:
         tax1 = 0.2 * 37500
         tax2 = 0.4 * 112500
         tax3 = 0.45 * (sal - 150000)
         total_tax = tax1 + tax2 + tax3
-        return str("%.2f" %total_tax), str("%.2f" %pension_contr) 
     
-    else:
-        return 'Unknown'
+    return total_tax, pension_contr
 
 def national_insurance_20(sal):
     """
@@ -67,21 +66,20 @@ def national_insurance_20(sal):
 
     returns
     -------
-    str: weekly national insurance payment
+    float: weekly national insurance payment
     """
     weekly = sal / 52
 
-    if int(weekly) in range(0, 184):
+    if int(weekly) < 0:
+        return 'Unknown'
+
+    elif int(weekly) in range(0, 184):
         ni = 0
-        return str("%.2f" %ni)
 
     elif int(weekly) in range(184, 963):
         ni = (weekly - 183) * 0.12
-        return str("%.2f" %ni)
-
-    elif int(weekly) > 962:
-        ni = (779 * 0.12) + ((weekly - 779 - 183) * 0.02)
-        return str("%.2f" %ni)
 
     else:
-        return 'Unknown'
+        ni = (779 * 0.12) + ((weekly - 779 - 183) * 0.02)
+ 
+    return ni
