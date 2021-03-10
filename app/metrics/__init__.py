af56a72e75d7ca5539f258daf2700c734ccdad7d
Charlie B <charlie@ubuntu>
2021-03-10 07:13:31 -0800

    add mongodb metrics

---
+++ app/metrics/__init__.py
@@ -0,0 +1,1 @@
+from .mongo_metrics import record_metrics
---
+++ app/metrics/mongo_metrics.py
@@ -0,0 +1,30 @@
+import pymongo
+from datetime import datetime
+
+def record_metrics():
+    try:
+        client = pymongo.MongoClient("mongodb:27017")
+        db = client['flask']
+        column = db['metrics']
+
+        today = str(datetime.today().date())
+
+        metrics = {
+            "date": today,
+            "count": 1
+        }
+
+        try:
+            result = column.find({"date": today})
+            new_count = str(int(result[0]['count']) + 1)
+            query = {"date": today}
+            new_values = {"$set": {"count": new_count}}
+            column.update_one(query, new_values)
+        except IndexError:
+            column.insert_one(metrics)
+
+    except Exception as e:
+        print(e)
+
+    finally:
+        client.close()
