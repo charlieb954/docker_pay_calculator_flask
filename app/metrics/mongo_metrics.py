0a2f16de6d5078b94657a343e10dd89b5e3cfc99
Charlie <charlie@Charlies-MBP.broadband>
2023-10-19 14:26:59 +0100

    black format

--- app/metrics/mongo_metrics.py
+++ app/metrics/mongo_metrics.py
@@ -1,22 +1,20 @@
 import pymongo
 from datetime import datetime
 
+
 def record_metrics():
     try:
         client = pymongo.MongoClient("mongodb:27017")
-        db = client['flask']
-        column = db['metrics']
+        db = client["flask"]
+        column = db["metrics"]
 
         today = str(datetime.today().date())
 
-        metrics = {
-            "date": today,
-            "count": 1
-        }
+        metrics = {"date": today, "count": 1}
 
         try:
             result = column.find({"date": today})
-            new_count = str(int(result[0]['count']) + 1)
+            new_count = str(int(result[0]["count"]) + 1)
             query = {"date": today}
             new_values = {"$set": {"count": new_count}}
             column.update_one(query, new_values)
