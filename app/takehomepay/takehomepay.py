fde5a788f1cba52ad4fa92a22bdfdf4f9bb7ae60
Charlie <charlie@Charlies-MBP.broadband>
2023-10-19 14:26:02 +0100

    black format

--- app/takehomepay/takehomepay.py
+++ app/takehomepay/takehomepay.py
@@ -1,12 +1,12 @@
 from takehomepay.utils import thousands_format
 
-__all__ = ['tax_pens_20',
-        'national_insurance_20']
+__all__ = ["tax_pens_20", "national_insurance_20"]
+
 
 def tax_pens_20(sal, pens):
     """
     calulate tax contributions for 2020-21 tax year.
-    
+
     parameters
     ----------
     sal = int: salary
@@ -16,34 +16,34 @@ def tax_pens_20(sal, pens):
     -------
     float: total_tax to pay, float: pension_contr to pay
     """
-    pension_contr = sal * (pens/100)
+    pension_contr = sal * (pens / 100)
     allowance = 12500
     sal = sal - pension_contr
 
     if sal < 0:
-        return 'Unknown'
+        return "Unknown"
 
     elif sal <= allowance:
         total_tax = 0
 
-    elif sal in range(12501,50001):
+    elif sal in range(12501, 50001):
         tax1 = 0 * allowance
         tax2 = 0.2 * (sal - allowance)
         total_tax = tax1 + tax2
 
-    elif sal in range(50001,100001):
+    elif sal in range(50001, 100001):
         tax1 = 0 * allowance
         tax2 = 0.2 * 37500
         tax3 = 0.4 * (sal - 50000)
         total_tax = tax1 + tax2 + tax3
 
-    elif sal in range(100001,125000):
+    elif sal in range(100001, 125000):
         allowance = (sal - 100000) / 2
         tax1 = 0.2 * 37500
         tax2 = 0.4 * (sal - (37500 + allowance))
         total_tax = tax1 + tax2
 
-    elif sal in range(125000,150001):
+    elif sal in range(125000, 150001):
         tax1 = 0.2 * 37500
         tax2 = 0.4 * (sal - 37500)
         total_tax = tax1 + tax2
@@ -53,13 +53,14 @@ def tax_pens_20(sal, pens):
         tax2 = 0.4 * 112500
         tax3 = 0.45 * (sal - 150000)
         total_tax = tax1 + tax2 + tax3
-    
+
     return total_tax, pension_contr
+
 
 def national_insurance_20(sal):
     """
     calulate national insurance contributions for 2020-21 tax year.
-    
+
     parameters
     ----------
     sal = int: salary
@@ -71,7 +72,7 @@ def national_insurance_20(sal):
     weekly = sal / 52
 
     if int(weekly) < 0:
-        return 'Unknown'
+        return "Unknown"
 
     elif int(weekly) in range(0, 184):
         ni = 0
@@ -81,5 +82,5 @@ def national_insurance_20(sal):
 
     else:
         ni = (779 * 0.12) + ((weekly - 779 - 183) * 0.02)
- 
+
     return ni
--- app/takehomepay/utils.py
+++ app/takehomepay/utils.py
@@ -1,5 +1,6 @@
 def thousands_format(num):
     return f"{num:,.2f}"
 
+
 def thousands_to_float(thousands):
     return float(thousands.replace(",", ""))
